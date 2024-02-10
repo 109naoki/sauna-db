8fb9522c2d31eea8b34154dc2f56df94cfd27cd3
argyle-ito <ito@argyle.jp>
2024-02-10 09:37:09 +0900

    Draft mode

--- app/articles/[slug]/page.tsx
+++ app/articles/[slug]/page.tsx
@@ -1,5 +1,7 @@
 import { getArticleBySlug, getArticles } from "@/lib/newt";
 import { Metadata } from "next";
+import { draftMode } from "next/headers";
+import Link from "next/link";
 
 type Props = {
   params: {
@@ -16,8 +18,9 @@ export async function generateStaticParams() {
 export const dynamicParams = false;
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { isEnabled } = draftMode();
   const { slug } = params;
-  const article = await getArticleBySlug(slug);
+  const article = await getArticleBySlug(slug, isEnabled);
 
   return {
     title: article?.title,
@@ -26,12 +29,18 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
 }
 
 export default async function Article({ params }: Props) {
+  const { isEnabled } = draftMode();
   const { slug } = params;
-  const article = await getArticleBySlug(slug);
+  const article = await getArticleBySlug(slug, isEnabled);
   if (!article) return;
 
   return (
     <main>
+      {isEnabled && (
+        <Link href="/api/disable-draft" prefetch={false}>
+          Draft Modeをやめる
+        </Link>
+      )}
       <h1>{article.title}</h1>
       <div dangerouslySetInnerHTML={{ __html: article.body }} />
     </main>
--- lib/newt.ts
+++ lib/newt.ts
@@ -2,16 +2,23 @@ import "server-only";
 import { createClient } from "newt-client-js";
 import { Article } from "@/types/article";
 import { cache } from "react";
-
-const client = createClient({
+// Newt CDN APIのクライアント（公開コンテンツのみ取得）
+const cdnClient = createClient({
   spaceUid: process.env.NEWT_SPACE_UID + "",
   token: process.env.NEWT_CDN_API_TOKEN + "",
   apiType: "cdn",
 });
 
+// Newt APIのクライアント（全コンテンツ取得）
+const apiClient = createClient({
+  spaceUid: process.env.NEWT_SPACE_UID + "",
+  token: process.env.NEWT_API_TOKEN + "",
+  apiType: "api",
+});
+
 // 一覧
 export const getArticles = cache(async () => {
-  const { items } = await client.getContents<Article>({
+  const { items } = await cdnClient.getContents<Article>({
     appUid: "blog",
     modelUid: "article",
     query: {
@@ -22,14 +29,17 @@ export const getArticles = cache(async () => {
 });
 
 // 詳細
-export const getArticleBySlug = cache(async (slug: string) => {
-  const article = await client.getFirstContent<Article>({
-    appUid: "blog",
-    modelUid: "article",
-    query: {
-      slug,
-      select: ["_id", "title", "slug", "body"],
-    },
-  });
-  return article;
-});
+export const getArticleBySlug = cache(
+  async (slug: string, isDraft: boolean) => {
+    const client = isDraft ? apiClient : cdnClient;
+    const article = await client.getFirstContent<Article>({
+      appUid: "blog",
+      modelUid: "article",
+      query: {
+        slug,
+        select: ["_id", "title", "slug", "body"],
+      },
+    });
+    return article;
+  }
+);
